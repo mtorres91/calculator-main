07126ad1b8b9ee8fc366970a3490a5b48e9f072e
mtorres91 <m.a.t.a.9125_@hotmail.com>
2022-10-24 17:50:46 -0500

    CHANGE URL

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 const constants = {
   defaultLanguage: 'es',
-  appUrl: 'https://of-loan-prequal.netlify.app',
+  appUrl: 'https://of-form.netlify.app/',
   submitUrl: 'https://www.formbackend.com/f/8d5eaa5dff45959d',
   websiteUrl: 'https://grape-tan-3sbh.squarespace.com/',
   version: '1.0.2',
